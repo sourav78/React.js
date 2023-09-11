841f4066f73a946fd686cfad8daeffaa526b3263
sourav78 <souravranjan488@gmail.com>
2023-09-11 23:58:37 +0530

    currency converter strt

---
+++ currency-converter/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1 className=' text-4xl text-purple-600'>
+          I am julu
+      </h1>
+    </>
+  );
+}
+
+export default App;
