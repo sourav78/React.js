abde6edc1d9ba184b339ff8d0c0aa5f4b905f183
sourav78 <souravranjan488@gmail.com>
2023-09-14 19:43:38 +0530

    react router

---
+++ react-router/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Footer from './Components/Footer/Footer';
+import Header from './Components/Header/Header';
+
+function App() {
+  return (
+    <>
+    </>
+  );
+}
+
+export default App;
---
+++ react-router/src/index.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import Layout from './Layout';
+import Home from './Components/Home/Home';
+import Header from './Components/Header/Header';
+import About from './Components/About/About';
+
+const router  = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "about",
+        element: <About />
+      }
+    ]
+  }
+])
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <RouterProvider router={router}/>
+  </React.StrictMode>
+);
+
