91b9d32820e2f88f6db7f5e09c3d60b4fd7c0c9e
sourav78 <souravranjan488@gmail.com>
2023-09-17 00:12:53 +0530

    context 1 created

---
+++ Context project 1/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Welcome to world of context API</h1>
+    </>
+  )
+}
+
+export default App
