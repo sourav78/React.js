f0febda2fff03563a89d353253d584c50d9a3a70
sourav78 <souravranjan488@gmail.com>
2023-09-20 15:21:37 +0530

    cc

--- Context Theme toggler/src/Components/Card.jsx
+++ Context Theme toggler/src/Components/Card.jsx
@@ -3,9 +3,11 @@ import React from 'react'
 export default function Card() {
     return (
         <div className="w-full bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="/">
-                <img className="p-8 rounded-t-lg" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
-            </a>
+            <div className=" p-8">
+                <a href="/">
+                    <img className=" rounded-lg dark:border-2 dark:border-white" src="https://images.pexels.com/photos/18264716/pexels-photo-18264716/free-photo-of-man-people-laptop-internet.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="product_image1" />
+                </a>
+            </div>
             <div className="px-5 pb-5">
                 <a href="/">
                     <h5 className="text-xl font-semibold tracking-tight text-gray-900 dark:text-white">
