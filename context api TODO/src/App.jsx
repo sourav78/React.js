9cab0c120913139f674b50ee2b787e068de5cc06
sourav78 <souravranjan488@gmail.com>
2023-10-21 14:02:02 +0530

    TODO Context api

--- context api TODO/src/App.jsx
+++ context api TODO/src/App.jsx
@@ -1,15 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useEffect, useState } from 'react'
+import { TodoProvider } from './context'
+import { TodoForm, TodoItem } from './components'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([])
+
+  const addTodo = (todo) => {
+    setTodos((prev) => [{id: Date.now(), ...todo}, ...prev])
+  }
+
+  const updateTodo = (id, todo) => {
+    setTodos((prev) => prev.map((eachTodo) => (eachTodo.id === id ? todo : eachTodo)))
+  }
+
+  const deleteTodo = (id) => {
+    setTodos((prev) => prev.filter((eachTodo) => eachTodo.id !== id))
+  }
+
+  const toggleComplete = (id) => {
+    setTodos((prev) => prev.map((eachTodo) => eachTodo.id === id ? {...eachTodo, completed: !eachTodo.completed} : eachTodo))
+  }
+
+  useEffect(() => {
+    const todos = JSON.parse(localStorage.getItem("todoItems"))
+
+    if(todos && todos.length > 0){
+      setTodos(todos)
+    }
+  }, [])
+
+  useEffect(() => {
+    localStorage.setItem("todoItems", JSON.stringify(todos))
+  }, [todos])
 
     return (
-      <>
-        <h1 className=' text-center text-green-500 underline'>Hello TODO</h1>
-      </>
+      <TodoProvider value={{todos, addTodo, updateTodo, deleteTodo, toggleComplete}}>
+          <div className="bg-[#172842] min-h-screen w-full py-8">
+              <div className="w-full max-w-2xl mx-auto shadow-md rounded-lg px-4 py-3 text-white">
+                  <h1 className="text-2xl font-bold text-center mb-8 mt-2">Manage Your Todos</h1>
+                  <div className="mb-4">
+                      <TodoForm/> 
+                  </div>
+                  <div className="flex flex-wrap gap-y-3">
+                      {todos.map((todo) => (
+                          <div key={todo.id} className='w-full'>
+                              <TodoItem todo={todo} />
+                          </div>
+                      ))}
+                  </div>
+              </div>
+          </div>
+      </TodoProvider>
     )
 }
 
