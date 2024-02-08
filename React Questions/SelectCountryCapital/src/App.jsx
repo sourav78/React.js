1e5479cbb98b0b982e0ff1d05b1a3d2a6ed45dac
sourav78 <souravranjan488@gmail.com>
2024-02-09 01:05:31 +0530

    selectcountrycapital optimize - react question

--- React Questions/SelectCountryCapital/src/App.jsx
+++ React Questions/SelectCountryCapital/src/App.jsx
@@ -5,6 +5,7 @@ import './App.css'
 
 function App() {
 
+
     const [allData, setAllData] = useState([])
     const [selectedData, setSelectedData] = useState([])
     const [checkValue, setCheckValue] = useState(false)
@@ -14,18 +15,13 @@ function App() {
         const data = Object.entries(datas).flat(2)
         const suffleData = _.shuffle(data)
         setAllData(suffleData)
-
-
     }, [])
 
     useEffect(() => {
         if(checkValue){
             let filteredData = allData.filter( item => !selectedData.includes(item))
-            console.log(filteredData);
             setAllData(filteredData)
             setSelectedData([])
-
-            
         }else{
             console.log("You selected the wrong country");
             setSelectedData([])
@@ -40,19 +36,10 @@ function App() {
 
         if (selectedValues.length === 2) {
 
-
             if (Object.keys(datas).includes(selectedValues[0])) {
-                if (selectedValues[1] === datas[selectedValues[0]]) {
-                    setCheckValue(true)
-                } else {
-                    setCheckValue(false)
-                }
+                selectedValues[1] === datas[selectedValues[0]] ? setCheckValue(true) : setCheckValue(false)
             } else {
-                if (selectedValues[0] === datas[selectedValues[1]]) {
-                    setCheckValue(true)
-                } else {
-                    setCheckValue(false)
-                }
+                selectedValues[0] === datas[selectedValues[1]] ? setCheckValue(true) : setCheckValue(false)
             }
 
             setTrigger(prev => !prev)
