69db567a2d7e8747be1b246e8c897801fd292397
sourav78 <souravranjan488@gmail.com>
2024-06-07 12:44:40 +0530

    public chat design in progress

--- sockets/client/src/App.jsx
+++ sockets/client/src/App.jsx
@@ -1,20 +1,36 @@
 import React, { useEffect, useMemo, useState } from 'react'
-import {io} from 'socket.io-client'
 
 import './App.css'
 import PublicChat from './components/PublicChat'
 
 const App = () => {
 
-  
+  const [components, setComponents] = useState(null)
 
-  const socket = useMemo(() => io("http://localhost:3000"), [])
-
-  
 
   return (
     <>
-      <PublicChat socket={socket}/>
+      {
+        !components && (
+          <div className="p-2 flex justify-center">
+            <button 
+              onClick={() => setComponents('public')}
+              className={`bg-green-400 p-2 rounded shadow-md border-2 ${components === 'public' && 'outline'} outline-2 outline-blue-500`}>Public chat</button>
+          </div>
+        )
+      }
+      {
+        components === 'public' && (
+          <>
+            <div className="p-2">
+              <button 
+                onClick={() => setComponents(null)}
+                className='bg-gray-200 py-2 px-4 rounded text-lg'>Back</button>
+              <PublicChat />
+            </div>
+          </>
+        )
+      }
     </>
   )
 }
--- sockets/client/src/components/PublicChat.jsx
+++ sockets/client/src/components/PublicChat.jsx
@@ -1,9 +1,13 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import { TextField, Button} from '@mui/material'
+import {io} from 'socket.io-client'
 
-const PublicChat = ({socket}) => {
+const PublicChat = () => {
 
   const [message, setMessage] = useState("")
+
+  
+  const socket = useMemo(() => io("http://localhost:3000"), [])
 
   const handleMessageSend = () => {
     if(message){
@@ -36,7 +40,15 @@ const PublicChat = ({socket}) => {
 
   return (
     <div className="main-cont">
-      <div className="cont">
+      <div className="w-[40%] px-1 py-2 border border-black rounded-lg m-auto">
+        <div className="mb-2 border-black p-1 h-[600px] flex flex-col overflow-y-auto ">
+          <div className="py-1 flex justify-start">
+            <div className="inline-block max-w-[70%] px-3 py-2 bg-green-300 rounded-r-xl">Hyy iam Julu </div>
+          </div>
+          <div className="py-1 flex justify-end">
+            <div className="inline-block max-w-[70%] px-3 py-2 bg-blue-300 rounded-l-xl ">Hyy iam Julu Lorem </div>
+          </div>
+        </div>
         <form>
           <TextField
             style={{ flexGrow: true, width: "100%" }}
@@ -49,9 +61,7 @@ const PublicChat = ({socket}) => {
             Send
           </Button>
         </form>
-        <div className="">
-            
-        </div>
+        
       </div>
     </div>
   );
